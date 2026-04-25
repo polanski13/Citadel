45598439e931d4321c34352d9a9f7bd09d98ab78
apolanski <a_polanski@wolfram.id>
2026-04-25 03:26:15 +0300

    Bump swift-nio-ssh pin to Termy identification commit

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/polanski13/swift-nio-ssh.git", revision: "a05e6bbe6b141ee68da3030e00275504c0595d4d"),
+        .package(url: "https://github.com/polanski13/swift-nio-ssh.git", revision: "0ce30be0389300275536057b7fa674e4edb3b407"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.81.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.0"),
